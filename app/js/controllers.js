73677f0cfa1cc1f3494ed3183a64095652704d0a
Lai Lion <LionLai@Laiteki-MacBook-Pro.local>
2014-02-15 14:05:24 +0800

    change auth

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,49 +2,44 @@
 
 /* Controllers */
 
-app.controller('blogCtrl', ['$scope', '$firebase','firebaseAuth', 'url', function($scope, $firebase, firebaseAuth, url) {
-	var ref = new Firebase(url);
-	$scope.blogs = $firebase(ref);
-	$scope.add = function () {
+app.controller('blogCtrl', ['$scope', '$firebase','firebaseAuth', 'blogService', function($scope, $firebase, firebaseAuth, blogService) {
+	$scope.auth = firebaseAuth;
+	$scope.blogs = blogService.get;
+	$scope.post = {};
+	$scope.mode = 'read';
+
+	$scope.reset = function(){
+		$scope.mode = 'read';
+		$scope.post = {
+			title: '新的部落格喔',
+			content: '我新增了一筆資料'
+		};
+	};
+
+	$scope.add = function(){
 		if(firebaseAuth.user){
-			$scope.blogs.$add({
-				title: '新的部落格喔',
-				content: '我新增了一筆資料'
-			});
+			$scope.blogs.$add($scope.post)
 		}else{
 			alert('你沒有權限');
 		}
 	};
+
+	$scope.modify = function(data){
+		$scope.mode = 'modify';
+		$scope.post = data;
+	};
+
+	$scope.confireChange = function(){
+		$scope.blogs.$update();
+	};
+
+	$scope.del = function(data){
+		$scope.blogs.$remove(data)
+	};
+
+	$scope.reset();
 }])
 
-.controller('LoginCtrl', ['$scope', 'firebaseAuth', 'url', function($scope, firebaseAuth, url) {
-	$scope.login = function(){
-		firebaseAuth.login();
-	};
-
-	$scope.logout = function(){
-		firebaseAuth.logout();
-	};
-
-	$scope.isLoggedIn = function() {
-    	return !!$scope.user;   
-	};
-
-	// src: Alex Vanston (https://coderwall.com/p/ngisma)
-	$scope.safeApply = function(fn) {
-	    var phase = this.$root.$$phase;
-	    if (phase == '$apply' || phase == '$digest') {
-	        if(fn && (typeof(fn) === 'function')) {
-	            fn();
-	        }
-	    } else {
-	        this.$apply(fn);
-	    }
-	};
-
-	$scope.$on('authEvent', function() {
-	    $scope.safeApply(function() {
-	        $scope.user = firebaseAuth.user;
-	    });
-	});
+.controller('LoginCtrl', ['$scope', 'firebaseAuth', function($scope, firebaseAuth) {
+	$scope.auth = firebaseAuth;
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,36 +6,18 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 app.value('version', '0.1');
-app.value('url', 'https://ngblog.firebaseio.com/blogs');
+app.value('url', 'https://popping-fire-7709.firebaseio.com/blogs');
 
-app.service('firebaseAuth', ['$rootScope', 'url', function($rootScope, url){
-	var auth = {},
-	    loginUser = null;
-
-	auth.broadcastAuthEvent = function() {
-	    $rootScope.$broadcast('authEvent');
-	};
-
+app.service('firebaseAuth', ['$rootScope', 'url', '$firebaseSimpleLogin', function($rootScope, url, $firebaseSimpleLogin){
 	var blogsRef = new Firebase(url);
-	auth.client = new FirebaseSimpleLogin(blogsRef, function(error, user) {
-	  if (error) {
-	    alert(error)
-	  } else if (user) {
-	  	auth.user = user;
-	  } else {
-	  	auth.user = null;
-	  }auth.broadcastAuthEvent();
-	});
-
-	auth.login = function(){
-		auth.client.login('facebook', {
-		  rememberMe: true
-		})
-	};
-
-	auth.logout = function(){
-		auth.client.logout();
-	};
-
+	var auth = $firebaseSimpleLogin(blogsRef);
 	return auth;
+}]);
+
+app.service('blogService', ['$firebase', 'firebaseAuth', 'url', function($firebase, firebaseAuth, url){
+	var blog = {};
+	var ref = new Firebase(url);	
+
+	blog.get = $firebase(ref);
+	return blog;
 }]);
